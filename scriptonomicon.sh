a219652e66e471cf0a098923975f34d819e9deb8
v4u6h4n <vaughan.m.johnston@gmail.com>
2023-12-29 18:38:50 +1100

    Update scriptonomicon.sh

--- scriptonomicon.sh
+++ scriptonomicon.sh
@@ -1,74 +1,111 @@
 #!/bin/bash
 
 while true; do
-    choice=$(whiptail --title "Menu" --menu "Choose an option" 15 60 7 \
-    "1" "List of Scripts" \
+    menu_main=$(whiptail --title "Scriptonomicon" --menu "Main Menu" 15 60 7 \
+    "1" "Start" \
     "2" "Search" \
-    "3" "Help" \
-    "4" "About" \
-    "5" "Random" \
-    "6" "Update" \
-    "7" "Quit" 3>&1 1>&2 2>&3)
+    "3" "About" \
+    "4" "Quit" 3>&1 1>&2 2>&3)
 
-    case $choice in
+    case $menu_main in
         1)
-            # Code for List of Scripts
-            # Replace this comment with the actual code
-            echo "You selected: List of Scripts"
-            ;;
-        2)
-            # Code for Search
-            # Replace this comment with the actual code
-            echo "You selected: Search"
-            ;;
-        3)
-            # Code for Help
-            # Replace this comment with the actual code
-            echo "You selected: Help"
+            menu_distro=$(whiptail --title "Scriptonomicon" --menu "Linux Distributions" 15 60 4 \
+            "1" "Arch" \
+            "2" "Debian" \
+            "3" "Back" 3>&1 1>&2 2>&3)
+
+            case $menu_distro in
+                1)
+                    menu_arch=$(whiptail --title "Scriptonomicon" --menu "Desktop Environments" 15 60 4 \
+                    "1" "None" \
+                    "2" "Gnome" \
+                    "3" "Hyprland" \
+                    "4" "KDE" \
+                    "5" "Back" 3>&1 1>&2 2>&3)
+
+                    case $menu_arch in
+                        4)
+                            menu_arch_kde=$(whiptail --title "Scriptonomicon" --menu "KDE Plasma" 15 60 4 \
+                            "1" "Install" \
+                            "2" "Devices" \
+                            "3" "Networking" \
+                            "4" "Virtualization" \
+                            "5" "Back" 3>&1 1>&2 2>&3)
+
+                            case $menu_arch_kde in
+                                1)
+                                    echo "Installing KDE Plasma"
+                                    ;;
+                                2)
+                                    menu_arch_kde_devices=$(whiptail --title "Scriptonomicon" --menu "Devices" 15 60 4 \
+                                    "1" "Printers" \
+                                    "2" "Storage Devices" \
+                                    "3" "Back" 3>&1 1>&2 2>&3)
+
+                                    case $menu_arch_kde_devices in
+                                        1)
+                                            menu_arch_kde_printers=$(whiptail --title "Scriptonomicon" --menu "Printers" 15 60 2 \
+                                            "1" "HP" \
+                                            "2" "Back" 3>&1 1>&2 2>&3)
+
+                                            case $menu_arch_kde_printers in
+                                                1)
+                                                    echo "
+                                                    # Created: 2023-12-29
+                                                    # Updated: 2023-12-29
+
+                                                    # Install packages.
+                                                    sudo pacman -S cups cups-pdf print-manager system-config-printer hplip
+                                                    # Install cups service.
+                                                    sudo systemctl enable cups
+                                                    # Replace 'size' with your preferred paper size(s).
+                                                    echo 'size' >> /etc/papersize
+                                                    echo 'size' >> /etc/paperspecs
+                                                    # Restart computer.
+                                                    "
+                                                    ;;
+                                                2)
+                                                    ;;
+                                                *)
+                                                    ;;
+                                            esac
+                                            ;;
+                                        2)
+                                            ;;
+                                        3)
+                                            ;;
+                                        *)
+                                            ;;
+                                    esac
+                                    ;;
+                                3)
+                                    ;;
+                                4)
+                                    ;;
+                                5)
+                                    ;;
+                                *)
+                                    echo "Invalid option"
+                                    ;;
+                            esac
+                            ;;
+                        5)
+                            ;;
+                        *)
+                            ;;
+                    esac
+                    ;;
+                3)
+                    ;;
+                *)
+                    ;;
+            esac
             ;;
         4)
-            # Code for About
-            # Replace this comment with the actual code
-            echo "You selected: About"
-            ;;
-        5)
-            # Code for Random
-            # Replace this comment with the actual code
-            echo "You selected: Random"
-            ;;
-        6)
-            # Code for Update
-            # Replace this comment with the actual code
-            echo "You selected: Update"
-            ;;
-        7)
             echo "Exiting."
             exit 0
             ;;
         *)
-            echo "Invalid option."
             ;;
     esac
 done
-
-
-#########################################
-# Arch | KDE
-#########################################
-
-#########################################
-# HP Printer
-#########################################
-
-echo"
-:::::::::::::::::::::::::
-:: Created: 2023-12-29 ::
-:: Updated: 2023-12-29 ::
-:::::::::::::::::::::::::
-
-sudo pacman -S cups cups-pdf print-manager system-config-printer hplip
-sudo systemctl enable cups
-echo 'A4' >> /etc/papersize
-echo 'A4' >> /etc/paperspecs
-Restart computer.
-"
